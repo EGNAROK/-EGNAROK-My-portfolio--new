2f11d45918abb27f9771b32a6571db6f761226e6
EGNAROK <serhii.machkovskiy@gmail.com>
2025-08-23 01:50:05 +0300

    Fixed

--- src/shared/components/AppIcon/icons/Contact.tsx
+++ src/shared/components/AppIcon/icons/Contact.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import type { SVGProps } from "react";
 const SvgContact = (props: SVGProps<SVGSVGElement>) => (
   <svg
--- src/shared/components/AppIcon/icons/FacebookIcon.tsx
+++ src/shared/components/AppIcon/icons/FacebookIcon.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import type { SVGProps } from "react";
 const SvgFacebookIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
