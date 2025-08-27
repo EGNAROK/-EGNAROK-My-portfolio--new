5e8717be57f2c29a83046cbbf051ec867e260caa
EGNAROK <serhii.machkovskiy@gmail.com>
2025-08-27 21:36:08 +0300

    fix

--- src/shared/hooks/useLanguage/useLanguage.ts
+++ src/shared/hooks/useLanguage/useLanguage.ts
@@ -1,10 +1,16 @@
+import { useEffect } from "react";
 import { setLanguage } from "../../../store/slices/languageSlice/languageSlice";
 import { useAppDispatch } from "../store/useAppDispatch";
 import { useAppSelector } from "../store/useAppSelector";
+import i18n from "../../../i18n";
 
 export const useLanguage = () => {
   const dispatch = useAppDispatch();
   const language = useAppSelector((state) => state.language.language);
+
+    useEffect(() => {
+    i18n.changeLanguage(language);
+  }, [language]);
 
   const changeLanguage = (lang: "en" | "uk") => {
     dispatch(setLanguage(lang));
--- src/shared/hooks/useTheme/useTheme.ts
+++ src/shared/hooks/useTheme/useTheme.ts
@@ -1,10 +1,20 @@
+import { useEffect } from "react";
 import { toggleTheme } from "../../../store/slices/themeSlice/themeSlice";
 import { useAppDispatch } from "../store/useAppDispatch";
 import { useAppSelector } from "../store/useAppSelector";
+import { darkModeThemePalette, lightModeThemePalette } from "../../../core/configs";
+import { applyTheme } from "../../../core/providers/applyTheme";
 
 export const useTheme = () => {
   const dispatch = useAppDispatch();
   const mode = useAppSelector((state) => state.theme.mode);
+
+    useEffect(() => {
+    document.documentElement.setAttribute("data-theme", mode);
+    const palette = mode === "light" ? lightModeThemePalette : darkModeThemePalette;
+    applyTheme(palette);
+  }, [mode]);
+
 
   const toggle = () => {
     dispatch(toggleTheme());
